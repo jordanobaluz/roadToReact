8f0025f60491a8b752a27be7d5e716d0ca4a59cc
JordanoBaluz <jordanolb95@hotmail.com>
2022-04-06 21:20:25 -0300

    refactored the search element, table and button element. Created the components and respective class

--- src/App.js
+++ src/App.js
@@ -67,39 +67,61 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <form>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={this.onSearchChange}
-          />
-        </form>
-        {list.filter(isSearched(searchTerm)).map((item) => {
-          // //define wrapping function outside the method, to pass only the defined function to the handler
-          // const onHandleDismiss = () => this.onDismiss(item.objectID);
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={() => this.onDismiss(item.objectID)}
-                  type="button">
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          );
-        })}
+        <Search value={searchTerm} onChange={this.onSearchChange}>
+          Search
+        </Search>
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
   }
 }
 
+class Search extends Component {
+  render() {
+    const { value, onChange, children } = this.props;
+    return (
+      <form>
+        {children}
+        <input type="text" value={value} onChange={onChange} />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map((item) => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+            </span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+class Button extends Component {
+  render() {
+    const { onClick, className = "", children } = this.props;
+
+    return (
+      <button onClick={onClick} className={className} type="button">
+        {children}
+      </button>
+    );
+  }
+}
 // function App() {
 //   return (
 //     <div className="App">
@@ -118,5 +140,36 @@ class App extends Component {
 //     </div>
 //   );
 // }
+
+{
+  /* <form>
+<input
+  type="text"
+  value={searchTerm}
+  onChange={this.onSearchChange}
+/>
+</form>
+{list.filter(isSearched(searchTerm)).map((item) => {
+// //define wrapping function outside the method, to pass only the defined function to the handler
+// const onHandleDismiss = () => this.onDismiss(item.objectID);
+return (
+  <div key={item.objectID}>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button
+        onClick={() => this.onDismiss(item.objectID)}
+        type="button">
+        Dismiss
+      </button>
+    </span>
+  </div>
+);
+})} */
+}
 
 export default App;
