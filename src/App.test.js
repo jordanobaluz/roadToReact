e0fba7ddaec98b7a75492b5dd072bf9ff87c7510
JordanoBaluz <jordanolb95@hotmail.com>
2022-04-18 21:29:51 -0300

    created a test for the button component with the snapshot of him

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
-import App, { Search } from "./App";
+import App, { Search, Button } from "./App";
 
 describe("App", () => {
   test("renders learn react link", () => {
@@ -26,6 +26,19 @@ describe("Search", () => {
 
   test("has a valid snapshot", () => {
     const component = renderer.create(<Search>Search</Search>);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Button", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Button>Give Me More</Button>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  test("has a valid snapshot", () => {
+    const component = renderer.create(<Button>Give Me More</Button>);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
