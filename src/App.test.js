3f8451d800dac8fde900aba96f5247bcf891adee
JordanoBaluz <jordanolb95@hotmail.com>
2022-04-18 21:35:24 -0300

    created a snapshot test for the Table component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
-import App, { Search, Button } from "./App";
+import App, { Search, Button, Table } from "./App";
 
 describe("App", () => {
   test("renders learn react link", () => {
@@ -39,6 +39,26 @@ describe("Button", () => {
   });
   test("has a valid snapshot", () => {
     const component = renderer.create(<Button>Give Me More</Button>);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Table", () => {
+  const props = {
+    list: [
+      { title: "1", author: "1", num_comments: 1, points: 2, objectID: "y" },
+      { title: "2", author: "2", num_comments: 1, points: 2, objectID: "z" },
+    ],
+  };
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Table {...props} />, div);
+  });
+
+  test("has a valid snapshot", () => {
+    const component = renderer.create(<Table {...props} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
